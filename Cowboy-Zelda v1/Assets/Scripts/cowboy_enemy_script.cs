a6121764d03e77233d8fa340a52a2ba1ed39d5fe
bmoya13 <71987706+bmoya13@users.noreply.github.com>
2020-10-01 19:14:13 -0700

    Added basic enemy movement script
    Started cowboy basic enemy unit script, script allows for a sprite to move left and right using triggers that can be placed.

---
+++ Cowboy-Zelda v1/Assets/Scripts/cowboy_enemy_script.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class cowboy_enemy_script : MonoBehaviour
+{
+    public float speed;
+
+    public bool MoveRight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // checks if moveright bol is True, if so, the enemy will begin moving right cycle
+        if (MoveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+            // transform.localScale = new Vector2(2, 2);             this code could be used to flip the sprite in appropriate direction, if needed
+        }
+        else // else if moveright is False, enemy will begin moving left cycle
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+            // transform.localScale = new Vector2(-2, 2);            see above
+        }
+    }
+    void OnTriggerEnter2D(Collider2D trig)   // This requires hidden turntriggers (look in my sample scene to see how they should be placed, allows patrol of an area
+    {
+        if(trig.gameObject.CompareTag("turntrigger"))   // if the trigger it hits has tag turntrigger, carries out following process
+        {
+            if (MoveRight)                              // has the sprite start moving the other way
+            {
+                MoveRight = false;
+            }
+            else
+            {
+                MoveRight = true;
+            }
+        }
+    }
+}
