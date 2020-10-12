f555058ab0645189b3e71d0f1d68c3be3d0f8455
EmergencyPlayer <lg007@outlook.com>
2020-10-11 23:01:09 -0700

    Started Shooting Mechanics
    Not finished yet, still working on shooting

---
+++ Cowboy-Zelda v1/Assets/Scripts/ShootingMech.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingMech : MonoBehaviour
+{
+    public GameObject crossHair;
+    public GameObject bulletPrefab;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0.0f);
+
+        
+
+        MoveCrossHair();
+
+        transform.position = transform.position + movement * Time.deltaTime;
+
+    }
+
+    private void MoveCrossHair(){
+        Vector3 aim = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0.0f);
+        aim.Normalize();
+    }
+}
