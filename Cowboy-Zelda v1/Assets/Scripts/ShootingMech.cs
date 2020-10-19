21934f1802d4ecf39ccc3d4d9134840f842b49e8
EmergencyPlayer <lg007@outlook.com>
2020-10-19 12:21:03 -0700

    Added shooting
    I was able to add a very basic shooting script, no hitboxes yet and it only works in mouse direction, I will continue to work on it to be based on character direciton

--- Cowboy-Zelda v1/Assets/Scripts/ShootingMech.cs
+++ Cowboy-Zelda v1/Assets/Scripts/ShootingMech.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class ShootingMech : MonoBehaviour
 {
-    public GameObject crossHair;
+    public float offset;
+
     public GameObject bulletPrefab;
-    
-    
+    public Transform shotPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,18 +19,19 @@ public class ShootingMech : MonoBehaviour
     void Update()
     {
         
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0.0f);
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0.0f);//moves character
+        transform.position = transform.position + movement * Time.deltaTime;
+
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;//makes bullet follow mouse
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f,0f,rotZ + offset);
+
+        if (Input.GetMouseButtonDown(0)){//button press to shoot
+            Instantiate(bulletPrefab, shotPoint.position, transform.rotation);
+        }
 
         
 
-        MoveCrossHair();
-
-        transform.position = transform.position + movement * Time.deltaTime;
-
     }
 
-    private void MoveCrossHair(){
-        Vector3 aim = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0.0f);
-        aim.Normalize();
-    }
 }
