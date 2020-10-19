5b9ea317db6e9c16c5876325c66e84fc24abc831
EmergencyPlayer <lg007@outlook.com>
2020-10-19 14:30:56 -0700

    Update to Shooting Mechanics
    Finally fixed the mouse issue, character should be shooting from direction now. Also changed the movement system, that code is included with the ShootingMech.cs script

--- Cowboy-Zelda v1/Assets/Scripts/ShootingMech.cs
+++ Cowboy-Zelda v1/Assets/Scripts/ShootingMech.cs
@@ -22,15 +22,29 @@ public class ShootingMech : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0.0f);//moves character
         transform.position = transform.position + movement * Time.deltaTime;
 
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;//makes bullet follow mouse
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f,0f,rotZ + offset);
+        //Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;//makes bullet follow mouse
+        //float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler(0f,0f,rotZ + offset);
 
-        if (Input.GetMouseButtonDown(0)){//button press to shoot
-            Instantiate(bulletPrefab, shotPoint.position, transform.rotation);
+
+
+        if(Input.GetAxis("Horizontal") != 0){
+            if(Input.GetAxis("Horizontal") > 0){
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }   else{
+            transform.rotation = Quaternion.Euler(0, -180, 0);
+            }
+        }   else if(Input.GetAxis("Vertical") != 0){
+                if(Input.GetAxis("Vertical") >= 0){
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                }   else{
+                    transform.rotation = Quaternion.Euler(-180, 0, 0);
+                }
         }
 
-        
+        if (Input.GetMouseButtonDown(0)){//button press to shoot
+            Instantiate(bulletPrefab, shotPoint.position, Quaternion.FromToRotation(new Vector3(1, 0, 0), movement));
+        }
 
     }
 
