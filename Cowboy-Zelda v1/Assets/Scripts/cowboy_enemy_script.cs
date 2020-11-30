469fe4f565591653b5fbbff669eb3eabb09c5311
EmergencyPlayer <EmergencyPlayer@users.noreply.github.com>
2020-11-29 18:42:48 -0800

    Working on enemy sprites
    Reworking enemy sprite to have it animate

--- Cowboy-Zelda v1/Assets/Scripts/cowboy_enemy_script.cs
+++ Cowboy-Zelda v1/Assets/Scripts/cowboy_enemy_script.cs
@@ -11,16 +11,31 @@ public class cowboy_enemy_script : MonoBehaviour
 
     public bool MoveRight;
 
+    public Animator animator;
+
+    private Transform target;
+
     void Start()
     {
         Invoke("ChangeDirection", movementTimer);
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 movement = new Vector3(x, y, 0.0f);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+
+
         // checks if moveright bol is True, if so, the enemy will begin moving right cycle
-        if (MoveRight)
+        /*if (MoveRight)
         {
             transform.Translate(2 * Time.deltaTime * speed, 0, 0);
             // transform.localScale = new Vector2(2, 2);             this code could be used to flip the sprite in appropriate direction, if needed
@@ -29,7 +44,10 @@ public class cowboy_enemy_script : MonoBehaviour
         {
             transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
             // transform.localScale = new Vector2(-2, 2);            see above
-        }
+        }*/
+
+
+
     }
     void OnTriggerEnter2D(Collider2D trig)   // This requires hidden turntriggers (look in my sample scene to see how they should be placed, allows patrol of an area
     {
